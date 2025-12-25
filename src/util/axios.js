5dc99aa198f4d73685e9b9cf4ec9e751b994eb2e
SandalaNithin <sandalanithinkumar123@gmail.com>
2025-12-25 14:41:50 +0530

    baseurl

--- src/util/axios.js
+++ src/util/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://server-1-ghbe.onrender.com",
+  baseURL: "http://localhost:5000",
 });
 
 export const sendBooking = (data) => API.post("/api/booking", data);
